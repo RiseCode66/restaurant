ea7f7e207e218c313a34ea523f6052a90b5290e6
RiseCode66 <124663679+RiseCode66@users.noreply.github.com>
2025-02-11 08:24:18 +0300

    dev

--- public/info.php
+++ public/info.php
@@ -1,4 +1,23 @@
 <?php
 // info.php
-$output = shell_exec('php ../bin/console router:match /api/client/get');
-echo "<pre>$output</pre>";
+
+// Chemin vers le fichier de log Symfony
+$logFile = '/var/www/html/var/log/dev.log';  // Modifie le chemin si nécessaire
+
+// Vérifie si le fichier existe
+if (file_exists($logFile)) {
+    // Ouvre le fichier pour le lire ligne par ligne
+    $handle = fopen($logFile, 'r');
+    if ($handle) {
+        echo "<pre>";  // Utilisé pour formater l'affichage
+        while (($line = fgets($handle)) !== false) {
+            echo htmlspecialchars($line) . "<br>";
+        }
+        fclose($handle);
+        echo "</pre>";
+    } else {
+        echo "Impossible de lire le fichier de log.";
+    }
+} else {
+    echo "Le fichier de log n'existe pas.";
+}
