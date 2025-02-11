f38c26aa785051568a1a57ed18ecea446209f408
RiseCode66 <124663679+RiseCode66@users.noreply.github.com>
2025-02-11 08:51:06 +0300

    rehthg

--- public/info.php
+++ public/info.php
@@ -1,16 +1,6 @@
 <?php
-// info.php
-
-// Chemin vers le fichier de log Symfony
-$logFile = '/var/log/faillog';  // Modifie le chemin si nécessaire
-
-// Vérifie si le fichier existe
-if (file_exists($logFile)) {
-    // Lis le contenu du fichier
-    $logContents = file_get_contents($logFile);
-
-    // Affiche les logs dans le navigateur
-    echo "<pre>$logContents</pre>";
-} else {
-    echo "Le fichier de log n'existe pas.";
-}
+echo "<pre>";
+print_r(scandir('/var/log/'));
+print_r(scandir('/var/www/html/public/'));
+echo "</pre>";
+?>
