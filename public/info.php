0058480df5d46d6c6945247c4e9d6dbaabf89198
RiseCode66 <124663679+RiseCode66@users.noreply.github.com>
2025-02-11 08:31:43 +0300

    5656526

--- public/info.php
+++ public/info.php
@@ -1,23 +1,6 @@
 <?php
-// info.php
-
-// Chemin vers le fichier de log Symfony
-$logFile = '/var/www/html/var/log/prod.log';  // Modifie le chemin si nécessaire
-
-// Vérifie si le fichier existe
-if (file_exists($logFile)) {
-    // Ouvre le fichier pour le lire ligne par ligne
-    $handle = fopen($logFile, 'r');
-    if ($handle) {
-        echo "<pre>";  // Utilisé pour formater l'affichage
-        while (($line = fgets($handle)) !== false) {
-            echo htmlspecialchars($line) . "<br>";
-        }
-        fclose($handle);
-        echo "</pre>";
-    } else {
-        echo "Impossible de lire le fichier de log.";
-    }
-} else {
-    echo "Le fichier de log n'existe pas.";
-}
+echo "<pre>";
+print_r(scandir('/var/www/var/log/'));
+print_r(scandir('/var/www/html/public/'));
+echo "</pre>";
+?>
