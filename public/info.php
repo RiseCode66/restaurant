7f6de22d475ff367a14f9898f3d322a90ea3100d
RiseCode66 <124663679+RiseCode66@users.noreply.github.com>
2025-02-11 08:11:28 +0300

    ugig

--- public/info.php
+++ public/info.php
@@ -1,4 +1,4 @@
 <?php
 // info.php
-$output = shell_exec('php bin/console router:match /api/client/get');
+$output = shell_exec('php ../bin/console router:match /api/client/get');
 echo "<pre>$output</pre>";
